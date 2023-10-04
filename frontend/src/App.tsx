88021b183aa0c349f29ef6eafef5d13e1280f134
Maxdev18 <maxpersonal1721@gmail.com>
2023-10-04 15:29:56 -0400

    Front end is set up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      This is the frontend!
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
