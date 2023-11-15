4283604f743825ba7f3f5b41bf096f3abda65940
Maxdev18 <maxpersonal1721@gmail.com>
2023-11-15 16:24:15 -0500

    Updated gitignore file

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 import json
-from flask import Flask, redirect, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import numpy
 
 app = Flask(__name__)
 cors = CORS(app, origins=["http://localhost:3000"])
@@ -18,17 +19,17 @@ def get_users():
 @app.route("/get-result", methods=['POST'])
 @cross_origin()
 def getResult():
-  res = request.get_json()
+  questions = request.get_json()
 
-  for question in res["questions"]:
+  for question in questions["questions"]:
     for input in question["inputs"]:
-      print("Value " + str(res["questions"].index(question) + 1) + ": " + input)
+      print("Value " + str(questions["questions"].index(question) + 1) + ": " + input)
 
   result = {
     "accuracy": "98%"
   }
 
-  return jsonify(result)
+  return json.dumps(result)
 
 if __name__ == "__main__":
   app.run()
